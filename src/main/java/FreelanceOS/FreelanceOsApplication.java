a3480ae8971d34afba68af999a8c4423336ba239
siddiqui-juber <siddiquejubair147@gmail.com>
2026-03-30 23:25:02 +0530

    Initial commit

---
+++ src/main/java/FreelanceOS/FreelanceOsApplication.java
@@ -0,0 +1,13 @@
+package FreelanceOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelanceOsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FreelanceOS/FreelanceOsApplicationTests.java
@@ -0,0 +1,13 @@
+package FreelanceOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
